e524c0f83c94680adb457b09b48161bf7576dcfc
Ludwig <lbayerlein@lbayerlein.de>
2016-03-22 19:01:25 +0100

    fedora update repo fix

--- manifests/updates.pp
+++ manifests/updates.pp
@@ -9,7 +9,7 @@ class repo_fedora::updates {
     $enabled = '0'
   }
   if $repo_fedora::enable_mirrorlist {
-    $mirrorlist = "${repo_fedora::mirrorlisturl}/metalink?repo=updates-released-f${repo_fedora::mirrorlist_tail}"
+    $mirrorlist = "${repo_fedora::mirrorlisturl}/metalink?repo=updates-released-f${::repo_fedora::releasever}&arch=\$basearch"
     $baseurl = 'absent'
   } else {
     $mirrorlist = 'absent'
@@ -21,13 +21,13 @@ class repo_fedora::updates {
     Yumrepo <| title == 'updates' |> { ensure => $repo_fedora::ensure_updates }
   }
 
-  yumrepo { 'updates':
+  yumrepo { 'fedora-updates':
     baseurl    => $baseurl,
     mirrorlist => $mirrorlist,
     descr      => 'Fedora $releasever - $basearch - Updates',
     enabled    => $enabled,
     gpgcheck   => '1',
-    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-f${repo_fedora::releasever}",
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-${repo_fedora::releasever}-\$basearch",
     #priority   => '1',
   }
 
