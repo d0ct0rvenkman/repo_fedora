7d54196a4630c06de13fa376b3206e2678d80fc6
Ludwig <lbayerlein@lbayerlein.de>
2016-03-22 19:26:26 +0100

    updates testing repo

---
+++ manifests/updates_testing.pp
@@ -0,0 +1,34 @@
+# The Fedora Updates repository
+class repo_fedora::updates_testing {
+
+  include repo_fedora
+
+  if $repo_fedora::enable_updates_testing {
+    $enabled = '1'
+  } else {
+    $enabled = '0'
+  }
+  if $repo_fedora::enable_mirrorlist {
+    $mirrorlist = "${repo_fedora::mirrorlisturl}/metalink?repo=updates-testing-f${::repo_fedora::releasever}&arch=\$basearch"
+    $baseurl = 'absent'
+  } else {
+    $mirrorlist = 'absent'
+    $baseurl = "${repo_fedora::repourl}/pub/fedora/linux/updates/testing/\$releasever/\$basearch/"
+  }
+
+  # Yumrepo ensure only in Puppet >= 3.5.0
+  if versioncmp($::puppetversion, '3.5.0') >= 0 {
+    Yumrepo <| title == 'fedora-updates-testing' |> { ensure => $repo_fedora::ensure_updates_testing }
+  }
+
+  yumrepo { 'fedora-updates-testing':
+    baseurl    => $baseurl,
+    mirrorlist => $mirrorlist,
+    descr      => 'Fedora $releasever - $basearch - Updates',
+    enabled    => $enabled,
+    gpgcheck   => '1',
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-${repo_fedora::releasever}-\$basearch",
+    #priority   => '1',
+  }
+
+}
