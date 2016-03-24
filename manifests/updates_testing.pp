8f1b93e198ec4d1bd09278dff9111614e21d3f45
Ludwig <lbayerlein@lbayerlein.de>
2016-03-24 16:14:14 +0100

    merge cleanup

--- manifests/updates_testing.pp
+++ manifests/updates_testing.pp
@@ -9,15 +9,6 @@ class repo_fedora::updates_testing {
     $enabled = '0'
   }
   if $repo_fedora::enable_mirrorlist {
-<<<<<<< HEAD
-    $mirrorlist = "${repo_fedora::mirrorlisturl}/metalink?repo=updates-\
-      testing-f${::repo_fedora::releasever}&arch=\$basearch"
-    $baseurl = 'absent'
-  } else {
-    $mirrorlist = 'absent'
-    $baseurl = "${repo_fedora::repourl}/pub/fedora/linux/updates/testing/\
-      \$releasever/\$basearch/"
-=======
     $mirrorlist = "${repo_fedora::mirrorlisturl}/metalink?repo=\
 updates-testing-f${::repo_fedora::releasever}&arch=\$basearch"
     $baseurl = 'absent'
@@ -25,16 +16,11 @@ updates-testing-f${::repo_fedora::releasever}&arch=\$basearch"
     $mirrorlist = 'absent'
     $baseurl = "${repo_fedora::repourl}/pub/fedora/linux/\
 updates/testing/\$releasever/\$basearch/"
->>>>>>> development
   }
 
   # Yumrepo ensure only in Puppet >= 3.5.0
   if versioncmp($::puppetversion, '3.5.0') >= 0 {
-<<<<<<< HEAD
-    Yumrepo <| title == 'fedora-updates-testing' |> \
-=======
     Yumrepo <| title == 'fedora-updates-testing' |>
->>>>>>> development
       { ensure => $repo_fedora::ensure_updates_testing }
   }
 
